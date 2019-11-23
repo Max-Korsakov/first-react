f22169e7cedc61d5b2a0aa06adc8c523cb098249
maxkorsakov <Maksim_Karsakou1@epam.com>
2019-11-23 15:04:08 +0300

    deployed version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-
-
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {Home} from './Pages/Home';
+import {About} from './Pages/About';
+import {Navbar} from './components/Navbar';
+import {Alert} from './components/Alert';
+import { AlertState } from './context/alert/AlertState';
+import { FirebaseState } from './context/firebase/FirebaseState';
+ 
 function App() {
   return (
-    <div className="contaner">
-        <h1>Test</h1>
+    <FirebaseState>
+    <AlertState>
+    <BrowserRouter>
+    <Navbar />
+    <div className="container pt-4">
+      <Alert />
+        <Switch>
+          <Route path={'/'} exact component = {Home}/>
+          <Route path={'/about'} exact component = {About}/>
+        </Switch>
     </div>
+    </BrowserRouter>
+    </AlertState>
+    </FirebaseState>
   );
 }
 
